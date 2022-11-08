7790eb392a7e055fb5f6cdb60adc7c9aa46819af
chowdhuryy-ifty <mchowdhuryy@binaryquest.com>
2022-11-08 14:24:30 +0600

    email authentication added.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from "next-auth/providers/google";
 import clientPromise from "../../../database/connectDB";
 import {MongoDBAdapter} from "@next-auth/mongodb-adapter"
+import EmailProvider from "next-auth/providers/email";
 
 export default NextAuth({
     providers: [
@@ -13,7 +14,19 @@ export default NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
+        }),
+        EmailProvider({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: process.env.EMAIL_SERVER_PORT,
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD
+                }
+            },
+            from: process.env.EMAIL_FROM,
+            subject: "User Email Signin"
+        }),
     ],
     pages: {
         signIn: '/auth/signin',
